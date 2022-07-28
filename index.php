50bbaaffd32d624d5ee0899344a491a3b5ea2ac0
natan22gt <natsuaz27@gmail.com>
2022-07-27 22:25:30 -0300

    inicio do projeto, uso da font awesome

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="css/fontawesome/css/all.css">
+  <title>fluxo de caixa</title>
+</head>
+
+<body>
+  <div class="container">
+    <header class="header">
+      <i class="fa-solid fa-cash-register"></i>
+    </header>
+  </div>
+</body>
+
+</html>
