37a7419f486bf5c2a00eb465ef2a81e8f8048d45
natan22gt <natsuaz27@gmail.com>
2022-08-03 22:25:02 -0300

    começo da estilização usando tailwind

--- index.php
+++ index.php
@@ -5,29 +5,29 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/main.css">
   <link rel="stylesheet" href="css/fontawesome/css/all.css">
+  <script src="https://cdn.tailwindcss.com"></script>
   <title>fluxo de caixa</title>
 </head>
 
 <body>
-  <div class="container">
-    <header class="header">
-      <i class="fa-solid fa-cash-register"></i>
+  <div class="w-screen h-screen overflow-hidden">
+    <header class="w-screen h-1/6 flex bg-gray-700 justify-center items-center">
+      <i class="fa-solid fa-cash-register text-white text-5xl"></i>
     </header>
-    <aside class="sidebar">
-      <button class="btn-aside">
+    <aside class="w-1/5 h-5/6 bg-black text-white">
+      <button class="w-full py-2 hover:bg-white hover:text-black duration-100">
         Cadastro
       </button>
-      <button class="btn-aside">
+      <button class="w-full py-2 hover:bg-white hover:text-black duration-100">
         Entradas e saidas
       </button>
-      <button class="btn-aside">
+      <button class="w-full py-2 hover:bg-white hover:text-black duration-100">
         Estoque
       </button>
     </aside>
-    <main class="content">
-      <iframe src="" frameborder="0" class="content-page"></iframe>
+    <main class="">
+      <iframe src="" frameborder="0" class=""></iframe>
     </main>
   </div>
 </body>
