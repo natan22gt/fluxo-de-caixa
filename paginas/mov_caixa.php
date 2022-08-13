cbb1c46bb96689a33499bf43c18617e38855ba1f
natan22gt <natsuaz27@gmail.com>
2022-08-12 21:24:11 -0300

    css pagina inicial concluida, inicio css mov-caixa

--- paginas/mov_caixa.php
+++ paginas/mov_caixa.php
@@ -5,77 +5,84 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/mov-caixa.css">
     <title>Document</title>
 </head>
 
 <body>
-    <form action="" method="">
-        <label for="caixa">Caixa: </label><br>
-        <input type="text" name="caixa"></input><br>
+    <div class="container-caixa">
+        <form action="" method="" class="form-venda">
+            <fieldset>
+                <label for="caixa">Caixa: </label>
+                <input type="text" name="caixa"></input>
+            </fieldset>
+            <fieldset>
+                <label for="data">Data: </label>
+                <input type="date" name="data"></input>
+            </fieldset>
+            <fieldset>
+                <label for="descricao">Descrição: </label>
+                <textarea name="descricao" id="" cols="30" rows="10"></textarea>
+            </fieldset>
+            <fieldset>
+                <label for="operacao">Operação: </label>
+                <input type="radio" name="operacao">Entrada de caixa</input>
+                <input type="radio" name="operacao">Saida de caixa</input>
+            </fieldset>
+            <fieldset>
+                <label for="valor">Valor</label>
+                <input type="text" name="valor">
+            </fieldset>
+            <fieldset class="align-center">
+                <input type="submit" value="Gravar">
+                <input type="reset" value="Reset">
+                <input type="button" value="Fechar" id="close">
+            </fieldset>
+        </form>
 
-        <label for="data">Data: </label><br>
-        <input type="date" name="data"></input><br>
+        <table class="data">
+            <thead>
+                <th>Data</th>
+                <th>Descrição</th>
+                <th>Valor</th>
+            </thead>
+            <tr>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+            </tr>
+        </table>
 
-        <label for="descricao">Descrição: </label><br>
-        <textarea name="descricao" id="" cols="30" rows="10"></textarea><br>
-
-        <label for="operacao">Operação: </label><br>
-        <input type="radio" name="operacao">Entrada de caixa</input><br>
-        <input type="radio" name="operacao">Saida de caixa</input><br>
-
-        <label for="valor">Valor</label><br>
-        <input type="text" name="valor"><br><br><br><br>
-
-        <input type="submit" value="Gravar">
-        <input type="reset" value="Reset">
-        <input type="button" value="Fechar" id="close">
-
-
-
-    </form>
-    <br><br><br>
-    <table>
-        <thead>
-            <th>Data</th>
-            <th>Descrição</th>
-            <th>Valor</th>
-        </thead>
-        <tr>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-        </tr>
-        <tr>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-        </tr>
-        <tr>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-        </tr>
-        <tr>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-        </tr>
-        <tr>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-        </tr>
-    </table>
-    <br><br><br>
-    <div class="resultado">
-        <label for="data-i">Data inicial: </label>
-        <input type="date" name="data-i">
-        <label for="cred-total">Crédito total: </label>
-        <input type="text" name="cred-total">
-        <label for="deb-total">Débito total: </label>
-        <input type="text" name="deb-total">
-        <label for="saldo-final">Saldo final: </label>
-        <input type="text" name="saldo-final">
+        <div class="result">
+            <label for="data-i">Data inicial: </label>
+            <input type="date" name="data-i">
+            <label for="cred-total">Crédito total: </label>
+            <input type="text" name="cred-total">
+            <label for="deb-total">Débito total: </label>
+            <input type="text" name="deb-total">
+            <label for="saldo-final">Saldo final: </label>
+            <input type="text" name="saldo-final">
+        </div>
     </div>
 </body>
 
